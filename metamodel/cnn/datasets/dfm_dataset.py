de20dab0817b65b9afc639c5abd75cb893f0e78f
martinspetlik <martin.spetlik>
2023-03-09 09:58:29 +0100

    fix dfm dataset

--- metamodel/cnn/datasets/dfm_dataset.py
+++ metamodel/cnn/datasets/dfm_dataset.py
@@ -41,15 +41,15 @@ class DFMDataset(Dataset):
 
             if os.path.isdir(os.path.join(self._data_dir, s_dir)):
                 sample_dir = os.path.join(self._data_dir, s_dir)
-
                 bulk_file = os.path.join(sample_dir, self._bulk_file_name)
                 fractures_file = os.path.join(sample_dir, self._fracture_file_name)
                 output_file = os.path.join(sample_dir, self._output_file_name)
 
-                if os.path.exists(bulk_file) and os.path.exists(fractures_file) and os.path.exists(output_file):
+                if os.path.exists(bulk_file) and os.path.exists(output_file):
                     self._bulk_file_paths.append(bulk_file)
-                    self._fracture_file_paths.append(fractures_file)
                     self._output_file_paths.append(output_file)
+                    if os.path.exists(fractures_file):
+                        self._fracture_file_paths.append(fractures_file)
                 else:
                     continue
 
@@ -57,7 +57,11 @@ class DFMDataset(Dataset):
         return len(self._bulk_file_paths)
 
     def __getitem__(self, idx):
-        bulk_path, fractures_path = self._bulk_file_paths[idx], self._fracture_file_paths[idx]
+        fractures_path = []
+        bulk_path = self._bulk_file_paths[idx]
+
+        if idx in self._fracture_file_paths:
+            fractures_path = self._fracture_file_paths[idx]
         output_path = self._output_file_paths[idx]
 
         if isinstance(bulk_path, (list, np.ndarray)):
@@ -68,24 +72,29 @@ class DFMDataset(Dataset):
             return new_dataset
 
         bulk_features = np.load(bulk_path)["data"]
-        fractures_features = np.load(fractures_path)["data"]
+
+        if len(fractures_path) > 0:
+            fractures_features = np.load(fractures_path)["data"]
+        else:
+            fractures_features = None
         output_features = np.load(output_path, allow_pickle=True)
 
         if self._two_dim:
             indices = [0,1,3]
             bulk_features = bulk_features[indices, ...]
-            fractures_features = fractures_features[indices, ...]
+            if fractures_features is not None:
+                fractures_features = fractures_features[indices, ...]
             output_features = output_features[indices, ...]
 
         bulk_features_shape = bulk_features.shape
 
         flatten_bulk_features = bulk_features.reshape(-1)
-        flatten_fracture_features = fractures_features.reshape(-1)
+        if fractures_features is not None:
+            flatten_fracture_features = fractures_features.reshape(-1)
+            not_nan_indices = np.argwhere(~np.isnan(flatten_fracture_features))
+            flatten_bulk_features[not_nan_indices] = flatten_fracture_features[not_nan_indices]
 
-        not_nan_indices = np.argwhere(~np.isnan(flatten_fracture_features))
-        flatten_bulk_features[not_nan_indices] = flatten_fracture_features[not_nan_indices]
         final_features = flatten_bulk_features.reshape(bulk_features_shape)
-
         final_features = torch.from_numpy(final_features)
         output_features = torch.from_numpy(output_features)
 
